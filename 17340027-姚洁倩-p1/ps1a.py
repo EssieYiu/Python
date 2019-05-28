fe3bb23a8179de3c1628a29a052a6a247045102d
EssieYiu <31929622+EssieYiu@users.noreply.github.com>
2019-05-28 23:10:32 +0800

    space cows
    interesting problem

--- 17340027-姚洁倩-p1/ps1a.py
+++ 17340027-姚洁倩-p1/ps1a.py
@@ -24,8 +24,16 @@ def load_cows(filename):
     Returns:
     a dictionary of cow name (string), weight (int) pairs
     """
-    # TODO: Your code here
-    pass
+    dic = {}
+    f = open(filename,'r')
+    for lines in f:
+        lines = lines.strip('\n')
+        data = lines.split(',')
+        if dic.get(data[0],0) == 0:
+            dic[data[0]] = int(data[1])
+    f.close()
+    return dic
+
 
 # Problem 2
 def greedy_cow_transport(cows,limit=10):
@@ -50,8 +58,27 @@ def greedy_cow_transport(cows,limit=10):
     transported on a particular trip and the overall list containing all the
     trips
     """
-    # TODO: Your code here
-    pass
+    my_list = [] #store the cows that still not be transported
+    for key in cows:
+        my_list.append((key,cows[key]))
+    my_list.sort(key= lambda x:x[1],reverse = True)
+    temp_list = [] #a single trip
+    trip_list = [] #return value
+    cur_weight = 0 #weight of a single trip
+    index = 0
+    while len(my_list):
+        if cur_weight + my_list[index][1] <= limit:
+            temp_list.append(my_list[index][0])
+            cur_weight = cur_weight+my_list[index][1]
+            my_list.pop(index)
+        else:
+            index = index + 1
+        if index == len(my_list):
+            trip_list.append(temp_list)
+            temp_list = []
+            cur_weight = 0
+            index = 0
+    return trip_list
 
 # Problem 3
 def brute_force_cow_transport(cows,limit=10):
@@ -75,9 +102,21 @@ def brute_force_cow_transport(cows,limit=10):
     transported on a particular trip and the overall list containing all the
     trips
     """
-    # TODO: Your code here
-    pass
-        
+    least_trip = ([],99999)#list containing tuples of partition and its trip cost
+    for partition in get_partitions(cows):
+        for single_trip in partition:
+            total_weight = 0
+            over_weight = False
+            for single_cow in single_trip:
+                total_weight = total_weight + cows[single_cow]
+                if total_weight > 10:
+                    over_weight = True
+                    break
+            if over_weight == True:
+                break
+        if over_weight == False and len(partition)<least_trip[1]:
+            least_trip = (partition,len(partition))
+    return least_trip[0]
 # Problem 4
 def compare_cow_transport_algorithms():
     """
@@ -92,5 +131,19 @@ def compare_cow_transport_algorithms():
     Returns:
     Does not return anything.
     """
-    # TODO: Your code here
-    pass
+    start = time.time()
+    cows = load_cows("ps1_cow_data.txt")
+    greedy = greedy_cow_transport(cows)
+    end = time.time()
+    print("Greedy:Time spent:",end-start)
+    print("minimun number of trip:",len(greedy))
+    print("solution:",greedy)
+
+    start = time.time()
+    brute = brute_force_cow_transport(cows)
+    end = time.time()
+    print("Brute force:Time spent:",end-start)
+    print("minimun number of trip:",len(brute))
+    print("solution:",brute)
+
+compare_cow_transport_algorithms()
---
+++ 17340027-姚洁倩-p1/test.py
@@ -0,0 +1,7 @@
+my_list = [0,1,2,3,4,5,6,7,8,9]
+index = 2
+my_list.pop(2)
+print(my_list)
+print(my_list[index])
+print(len(my_list))
+print(my_list[len(my_list)])
