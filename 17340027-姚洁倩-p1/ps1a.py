802dbf24ddebf0e716511015c70b02c3a8a459f1
EssieYiu <31929622+EssieYiu@users.noreply.github.com>
2019-06-01 12:33:37 +0800

    ps2-6.0002
    ps2-6.0002

--- 17340027-姚洁倩-p1/ps1a.py
+++ 17340027-姚洁倩-p1/ps1a.py
@@ -13,17 +13,6 @@ import time
 
 # Problem 1
 def load_cows(filename):
-    """
-    Read the contents of the given file.  Assumes the file contents contain
-    data in the form of comma-separated cow name, weight pairs, and return a
-    dictionary containing cow names as keys and corresponding weights as values.
-
-    Parameters:
-    filename - the name of the data file as a string
-
-    Returns:
-    a dictionary of cow name (string), weight (int) pairs
-    """
     dic = {}
     f = open(filename,'r')
     for lines in f:
@@ -37,27 +26,6 @@ def load_cows(filename):
 
 # Problem 2
 def greedy_cow_transport(cows,limit=10):
-    """
-    Uses a greedy heuristic to determine an allocation of cows that attempts to
-    minimize the number of spaceship trips needed to transport all the cows. The
-    returned allocation of cows may or may not be optimal.
-    The greedy heuristic should follow the following method:
-
-    1. As long as the current trip can fit another cow, add the largest cow that will fit
-        to the trip
-    2. Once the trip is full, begin a new trip to transport the remaining cows
-
-    Does not mutate the given dictionary of cows.
-
-    Parameters:
-    cows - a dictionary of name (string), weight (int) pairs
-    limit - weight limit of the spaceship (an int)
-    
-    Returns:
-    A list of lists, with each inner list containing the names of cows
-    transported on a particular trip and the overall list containing all the
-    trips
-    """
     my_list = [] #store the cows that still not be transported
     for key in cows:
         my_list.append((key,cows[key]))
@@ -82,26 +50,6 @@ def greedy_cow_transport(cows,limit=10):
 
 # Problem 3
 def brute_force_cow_transport(cows,limit=10):
-    """
-    Finds the allocation of cows that minimizes the number of spaceship trips
-    via brute force.  The brute force algorithm should follow the following method:
-
-    1. Enumerate all possible ways that the cows can be divided into separate trips 
-        Use the given get_partitions function in ps1_partition.py to help you!
-    2. Select the allocation that minimizes the number of trips without making any trip
-        that does not obey the weight limitation
-            
-    Does not mutate the given dictionary of cows.
-
-    Parameters:
-    cows - a dictionary of name (string), weight (int) pairs
-    limit - weight limit of the spaceship (an int)
-    
-    Returns:
-    A list of lists, with each inner list containing the names of cows
-    transported on a particular trip and the overall list containing all the
-    trips
-    """
     least_trip = ([],99999)#list containing tuples of partition and its trip cost
     for partition in get_partitions(cows):
         for single_trip in partition:
@@ -119,20 +67,8 @@ def brute_force_cow_transport(cows,limit=10):
     return least_trip[0]
 # Problem 4
 def compare_cow_transport_algorithms():
-    """
-    Using the data from ps1_cow_data.txt and the specified weight limit, run your
-    greedy_cow_transport and brute_force_cow_transport functions here. Use the
-    default weight limits of 10 for both greedy_cow_transport and
-    brute_force_cow_transport.
-    
-    Print out the number of trips returned by each method, and how long each
-    method takes to run in seconds.
-
-    Returns:
-    Does not return anything.
-    """
     start = time.time()
-    cows = load_cows("ps1_cow_data.txt")
+    cows = load_cows("ps1_cow_data_2.txt")
     greedy = greedy_cow_transport(cows)
     end = time.time()
     print("Greedy:Time spent:",end-start)
--- 17340027-姚洁倩-p1/ps1b.py
+++ 17340027-姚洁倩-p1/ps1b.py
@@ -10,21 +10,27 @@
 #================================
 
 # Problem 1
+import copy
 def dp_make_weight(egg_weights, target_weight, memo = {}):
-    """
-    Find number of eggs to bring back, using the smallest number of eggs. Assumes there is
-    an infinite supply of eggs of each weight, and there is always a egg of value 1.
-    
-    Parameters:
-    egg_weights - tuple of integers, available egg weights sorted from smallest to largest value (1 = d1 < d2 < ... < dk)
-    target_weight - int, amount of weight we want to find eggs to fit
-    memo - dictionary, OPTIONAL parameter for memoization (you may not need to use this parameter depending on your implementation)
-    
-    Returns: int, smallest number of eggs needed to make target weight
-    """
-    # TODO: Your code here
-    pass
-
+    dp_egg = []
+    combination = []
+    for i in range(1000):
+        dp_egg.append(-1)
+    dp_egg[0] = 0
+    combination.append((0,[0]))
+    for w in range(1,target_weight+1):
+        min_num = 9999
+        for egg in egg_weights:
+            if egg <= w:
+                temp_num = dp_egg[w-egg] + 1 #状态转移方程
+                if temp_num < min_num:
+                    min_num = temp_num
+                    add_egg = egg
+                    temp_comb = copy.deepcopy(combination[w-egg][1])
+                    temp_comb.append(add_egg)
+        dp_egg[w] = min_num
+        combination.append((w,temp_comb))
+    return dp_egg[w]
 # EXAMPLE TESTING CODE, feel free to add more if you'd like
 if __name__ == '__main__':
     egg_weights = (1, 5, 10, 25)
@@ -32,5 +38,21 @@ if __name__ == '__main__':
     print("Egg weights = (1, 5, 10, 25)")
     print("n = 99")
     print("Expected ouput: 9 (3 * 25 + 2 * 10 + 4 * 1 = 99)")
+    print("Actual output:", dp_make_weight(egg_weights, n))
+    print()
+
+    egg_weights = (1, 5, 10, 50)
+    n = 100
+    print("Egg weights = (1, 50, 95)")
+    print("n = 100")
+    print("Expected ouput: 2 (2 * 50 = 100)")
+    print("Actual output:", dp_make_weight(egg_weights, n))
+    print()
+
+    egg_weights = (1, 9, 25, 30, 35, 40, 45, 92)
+    n = 98
+    print("Egg weights = (1, 9, 25, 30, 35, 40, 45, 92)")
+    print("n = 98")
+    print("Expected ouput: 4 (9 * 2 + 35 + 45 = 98)")
     print("Actual output:", dp_make_weight(egg_weights, n))
     print()
