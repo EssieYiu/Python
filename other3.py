9f2e66882bfe1ddb8d98f213c5e227e0f3148f6e
Erout <31929622+Erout@users.noreply.github.com>
2019-07-17 23:44:47 +0800

    Update other3.py
    m

--- other3.py
+++ other3.py
@@ -1,12 +1,45 @@
-dataset3['day_of_week'] = dataset3.date_received.astype('str').apply(lambda x:date(int(x[0:4]),int(x[4:6]),int(x[6:8])).weekday()+1)
-dataset3['day_of_month'] = dataset3.date_received.astype('str').apply(lambda x:int(x[6:8]))
-dataset3['days_distance'] = dataset3.date_received.astype('str').apply(lambda x:(date(int(x[0:4]),int(x[4:6]),int(x[6:8]))-date(2016,6,30)).days)
-dataset3['discount_man'] = dataset3.discount_rate.apply(get_discount_man)
-dataset3['discount_jian'] = dataset3.discount_rate.apply(get_discount_jian)
-dataset3['is_man_jian'] = dataset3.discount_rate.apply(is_man_jian)
-dataset3['discount_rate'] = dataset3.discount_rate.apply(calc_discount_rate)
-d = dataset3[['coupon_id']]
-d['coupon_count'] = 1
-d = d.groupby('coupon_id').agg('sum').reset_index()
-dataset3 = pd.merge(dataset3,d,on='coupon_id',how='left')
-dataset3.to_csv('data/coupon3_feature.csv',index=None)
+merchant3 = feature3[['merchant_id','coupon_id','distance','date_received','date']]
+
+t = merchant3[['merchant_id']]
+t.drop_duplicates(inplace=True)
+
+t1 = merchant3[merchant3.date!='null'][['merchant_id']]
+t1['total_sales'] = 1
+t1 = t1.groupby('merchant_id').agg('sum').reset_index()
+
+t2 = merchant3[(merchant3.date!='null')&(merchant3.coupon_id!='null')][['merchant_id']]
+t2['sales_use_coupon'] = 1
+t2 = t2.groupby('merchant_id').agg('sum').reset_index()
+
+t3 = merchant3[merchant3.coupon_id!='null'][['merchant_id']]
+t3['total_coupon'] = 1
+t3 = t3.groupby('merchant_id').agg('sum').reset_index()
+
+t4 = merchant3[(merchant3.date!='null')&(merchant3.coupon_id!='null')][['merchant_id','distance']]
+t4.replace('null',-1,inplace=True)
+t4.distance = t4.distance.astype('int')
+t4.replace(-1,np.nan,inplace=True)
+t5 = t4.groupby('merchant_id').agg('min').reset_index()
+t5.rename(columns={'distance':'merchant_min_distance'},inplace=True)
+
+t6 = t4.groupby('merchant_id').agg('max').reset_index()
+t6.rename(columns={'distance':'merchant_max_distance'},inplace=True)
+
+t7 = t4.groupby('merchant_id').agg('mean').reset_index()
+t7.rename(columns={'distance':'merchant_mean_distance'},inplace=True)
+
+t8 = t4.groupby('merchant_id').agg('median').reset_index()
+t8.rename(columns={'distance':'merchant_median_distance'},inplace=True)
+
+merchant3_feature = pd.merge(t,t1,on='merchant_id',how='left')
+merchant3_feature = pd.merge(merchant3_feature,t2,on='merchant_id',how='left')
+merchant3_feature = pd.merge(merchant3_feature,t3,on='merchant_id',how='left')
+merchant3_feature = pd.merge(merchant3_feature,t5,on='merchant_id',how='left')
+merchant3_feature = pd.merge(merchant3_feature,t6,on='merchant_id',how='left')
+merchant3_feature = pd.merge(merchant3_feature,t7,on='merchant_id',how='left')
+merchant3_feature = pd.merge(merchant3_feature,t8,on='merchant_id',how='left')
+merchant3_feature.sales_use_coupon = merchant3_feature.sales_use_coupon.replace(np.nan,0) #fillna with 0
+merchant3_feature['merchant_coupon_transfer_rate'] = merchant3_feature.sales_use_coupon.astype('float') / merchant3_feature.total_coupon
+merchant3_feature['coupon_rate'] = merchant3_feature.sales_use_coupon.astype('float') / merchant3_feature.total_sales
+merchant3_feature.total_coupon = merchant3_feature.total_coupon.replace(np.nan,0) #fillna with 0
+merchant3_feature.to_csv('data/merchant3_feature.csv',index=None)
